673b3a3494534907b22a346ae1baeeab471c6307
Čipl Jan (240600) <240600@vutbr.cz>
2024-10-29 16:41:53 +0100

    cv3

---
+++ cv03/.metadata/.plugins/org.eclipse.core.resources/.history/c0/a0944c020996001f1f39e180f744b141
@@ -0,0 +1,13 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 29, 2024
+ *      Author: 240600
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+
+#endif /* SCT_H_ */
---
+++ cv03/LED display/Core/Src/sct.c
@@ -0,0 +1,21 @@
+/*
+ * sct.c
+ *
+ *  Created on: Oct 29, 2024
+ *      Author: 240600
+ */
+
+
+#include "main.h"
+#include "sct.h"
+
+void sct_init(void){
+	sct_led(0);
+}
+
+
+void sct_led(uint32_t value){
+
+
+
+}
