445f0bca5b38d173746308ea51bad6dc9113d4d7
Čipl Jan (240600) <240600@vutbr.cz>
2024-11-06 10:23:56 +0100

    adding cv 8

--- freeARTOS/Core/Src/main.c
+++ freeARTOS/Core/Src/main.c
@@ -18,11 +18,12 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "sct.h"
-#include "1wire.h"
+#include "lis2dw12_reg.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -32,7 +33,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define CONVERT_T_DELAY 750
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -41,10 +42,14 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc;
+I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef huart2;
 
+osThreadId defaultTaskHandle;
+osThreadId VisualTaskHandle;
+osThreadId AcceleroTaskHandle;
+osMessageQId xVisualQueueHandle;
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -53,14 +58,42 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_ADC_Init(void);
+static void MX_I2C1_Init(void);
+void StartDefaultTask(void const *argument);
+void StartVisualTask(void const *argument);
+void StartAcceleroTask(void const *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
+		uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+		uint16_t len);
+static stmdev_ctx_t lis2dw12 = { .write_reg = platform_write, .read_reg =
+		platform_read, .handle = &hi2c1, };
 
+static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
+		uint16_t len) {
+	HAL_I2C_Mem_Write(handle, LIS2DW12_I2C_ADD_H, reg,
+	I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+	return 0;
+}
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+		uint16_t len) {
+	HAL_I2C_Mem_Read(handle, LIS2DW12_I2C_ADD_H, reg,
+	I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+	return 0;
+}
+
+int _write(int file, char const *buf, int n) {
+	/* stdout redirection to UART2 */
+	HAL_UART_Transmit(&huart2, (uint8_t*) (buf), n, HAL_MAX_DELAY);
+	return n;
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,57 +125,57 @@ int main(void) {
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_USART2_UART_Init();
-	MX_ADC_Init();
+	MX_I2C1_Init();
 	/* USER CODE BEGIN 2 */
-	sct_init();
-	OWInit();
-	HAL_ADCEx_Calibration_Start(&hadc);
-	HAL_ADC_Start(&hadc);
+
 	/* USER CODE END 2 */
+
+	/* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+	/* USER CODE END RTOS_MUTEX */
+
+	/* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+	/* USER CODE END RTOS_SEMAPHORES */
+
+	/* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+	/* USER CODE END RTOS_TIMERS */
+
+	/* Create the queue(s) */
+	/* definition and creation of xVisualQueue */
+	osMessageQDef(xVisualQueue, 16, uint16_t);
+	xVisualQueueHandle = osMessageCreate(osMessageQ(xVisualQueue), NULL);
+
+	/* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+	/* USER CODE END RTOS_QUEUES */
+
+	/* Create the thread(s) */
+	/* definition and creation of defaultTask */
+	osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+	defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+	/* definition and creation of VisualTask */
+	osThreadDef(VisualTask, StartVisualTask, osPriorityIdle, 0, 128);
+	VisualTaskHandle = osThreadCreate(osThread(VisualTask), NULL);
+
+	/* definition and creation of AcceleroTask */
+	osThreadDef(AcceleroTask, StartAcceleroTask, osPriorityIdle, 0, 128);
+	AcceleroTaskHandle = osThreadCreate(osThread(AcceleroTask), NULL);
+
+	/* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+	/* USER CODE END RTOS_THREADS */
+
+	/* Start scheduler */
+	osKernelStart();
+
+	/* We should never get here as control is now taken by the scheduler */
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
-		static int16_t temp18;
-		OWConvertAll();
-		HAL_Delay(CONVERT_T_DELAY);
-		OWReadTemperature(&temp18);
-
-		static const int16_t ntc_lookup[] = {
-#include "data.dlm"
-				};
-
-		uint16_t ad1 = HAL_ADC_GetValue(&hadc);
-
-		//sct_value(ntc_lookup[ad1]);
-		//HAL_Delay(500);
-
-		static enum {
-			S1, S2
-		} choise;
-		if (HAL_GPIO_ReadPin(S1_GPIO_Port, S1_Pin) == 0) {
-			choise = S1;
-		}
-
-		if (HAL_GPIO_ReadPin(S2_GPIO_Port, S2_Pin) == 0) {
-			choise = S2;
-		}
-
-		switch (choise) {
-		case S1:
-			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
-			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 1);
-			sct_value(temp18 / 10);
-			break;
-
-		case S2:
-			sct_value(ntc_lookup[ad1]);
-			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
-			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
-			break;
-		}
-		HAL_Delay(500);
-
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
@@ -157,16 +190,14 @@ int main(void) {
 void SystemClock_Config(void) {
 	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
 	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
 	/** Initializes the RCC Oscillators according to the specified parameters
 	 * in the RCC_OscInitTypeDef structure.
 	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI
-			| RCC_OSCILLATORTYPE_HSI14;
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
 	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
 	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.HSI14CalibrationValue = 16;
 	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
 	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
 	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
@@ -186,56 +217,55 @@ void SystemClock_Config(void) {
 	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
 		Error_Handler();
 	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+	PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
- * @brief ADC Initialization Function
+ * @brief I2C1 Initialization Function
  * @param None
  * @retval None
  */
-static void MX_ADC_Init(void) {
+static void MX_I2C1_Init(void) {
 
-	/* USER CODE BEGIN ADC_Init 0 */
+	/* USER CODE BEGIN I2C1_Init 0 */
 
-	/* USER CODE END ADC_Init 0 */
+	/* USER CODE END I2C1_Init 0 */
 
-	ADC_ChannelConfTypeDef sConfig = { 0 };
+	/* USER CODE BEGIN I2C1_Init 1 */
 
-	/* USER CODE BEGIN ADC_Init 1 */
-
-	/* USER CODE END ADC_Init 1 */
-
-	/** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-	 */
-	hadc.Instance = ADC1;
-	hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-	hadc.Init.Resolution = ADC_RESOLUTION_10B;
-	hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-	hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
-	hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-	hadc.Init.LowPowerAutoWait = DISABLE;
-	hadc.Init.LowPowerAutoPowerOff = DISABLE;
-	hadc.Init.ContinuousConvMode = ENABLE;
-	hadc.Init.DiscontinuousConvMode = DISABLE;
-	hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-	hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-	hadc.Init.DMAContinuousRequests = DISABLE;
-	hadc.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
-	if (HAL_ADC_Init(&hadc) != HAL_OK) {
+	/* USER CODE END I2C1_Init 1 */
+	hi2c1.Instance = I2C1;
+	hi2c1.Init.Timing = 0x00201D2B;
+	hi2c1.Init.OwnAddress1 = 0;
+	hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	hi2c1.Init.OwnAddress2 = 0;
+	hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+	hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
 		Error_Handler();
 	}
 
-	/** Configure for the selected ADC regular channel to be converted.
+	/** Configure Analogue filter
 	 */
-	sConfig.Channel = ADC_CHANNEL_1;
-	sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
-	sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-	if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK) {
+	if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE)
+			!= HAL_OK) {
 		Error_Handler();
 	}
-	/* USER CODE BEGIN ADC_Init 2 */
 
-	/* USER CODE END ADC_Init 2 */
+	/** Configure Digital filter
+	 */
+	if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN I2C1_Init 2 */
+
+	/* USER CODE END I2C1_Init 2 */
 
 }
 
@@ -292,24 +322,13 @@ static void MX_GPIO_Init(void) {
 	HAL_GPIO_WritePin(GPIOA, LED1_Pin | LD2_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB,
-			LED2_Pin | SCT_NOE_Pin | SCT_CLK_Pin | SCT_SDI_Pin | SCT_NLA_Pin,
-			GPIO_PIN_RESET);
-
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(DQ_GPIO_Port, DQ_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin : B1_Pin */
 	GPIO_InitStruct.Pin = B1_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-	/*Configure GPIO pins : S2_Pin S1_Pin */
-	GPIO_InitStruct.Pin = S2_Pin | S1_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 	/*Configure GPIO pins : LED1_Pin LD2_Pin */
 	GPIO_InitStruct.Pin = LED1_Pin | LD2_Pin;
@@ -318,21 +337,12 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : LED2_Pin SCT_NOE_Pin SCT_CLK_Pin SCT_SDI_Pin
-	 SCT_NLA_Pin */
-	GPIO_InitStruct.Pin = LED2_Pin | SCT_NOE_Pin | SCT_CLK_Pin | SCT_SDI_Pin
-			| SCT_NLA_Pin;
+	/*Configure GPIO pin : LED2_Pin */
+	GPIO_InitStruct.Pin = LED2_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-	/*Configure GPIO pin : DQ_Pin */
-	GPIO_InitStruct.Pin = DQ_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(DQ_GPIO_Port, &GPIO_InitStruct);
+	HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
 
 	/* USER CODE BEGIN MX_GPIO_Init_2 */
 	/* USER CODE END MX_GPIO_Init_2 */
@@ -342,6 +352,99 @@ static void MX_GPIO_Init(void) {
 
 /* USER CODE END 4 */
 
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const *argument) {
+	/* USER CODE BEGIN 5 */
+	/* Infinite loop */
+	for (;;) {
+
+		osDelay(1);
+	}
+	/* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartVisualTask */
+/**
+ * @brief Function implementing the VisualTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartVisualTask */
+void StartVisualTask(void const *argument) {
+	/* USER CODE BEGIN StartVisualTask */
+	/* Infinite loop */
+	for (;;) {
+
+		int16_t msg;
+
+		if (xQueueReceive(xVisualQueueHandle, &msg, portMAX_DELAY)) {
+			if (msg < -1000) {
+				HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+			} else if (msg > 1000) {
+				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 1);
+				HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+			} else {
+				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+				HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+			}
+		}
+
+	}
+	/* USER CODE END StartVisualTask */
+}
+
+/* USER CODE BEGIN Header_StartAcceleroTask */
+/**
+ * @brief Function implementing the AcceleroTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartAcceleroTask */
+void StartAcceleroTask(void const *argument) {
+	/* USER CODE BEGIN StartAcceleroTask */
+	/* Infinite loop */
+
+	/*toto je do predesleho cviceni blikani led*/
+
+	uint8_t whoamI = 0;
+	lis2dw12_device_id_get(&lis2dw12, &whoamI);
+	printf("LIS2DW12_ID %s\n", (whoamI == LIS2DW12_ID) ? "OK" : "FAIL");
+
+	lis2dw12_full_scale_set(&lis2dw12, LIS2DW12_2g);
+	lis2dw12_power_mode_set(&lis2dw12, LIS2DW12_CONT_LOW_PWR_LOW_NOISE_2);
+	lis2dw12_block_data_update_set(&lis2dw12, PROPERTY_ENABLE);
+	// enable continuous FIFO
+	lis2dw12_fifo_mode_set(&lis2dw12, LIS2DW12_STREAM_MODE);
+	// enable part from power-down
+	lis2dw12_data_rate_set(&lis2dw12, LIS2DW12_XL_ODR_25Hz);
+	for (;;) {
+
+		// Check device ID
+		uint8_t samples;
+		int16_t raw_acceleration[3];
+		lis2dw12_fifo_data_level_get(&lis2dw12, &samples);
+		for (uint8_t i = 0; i < samples; i++) {
+			// Read acceleration data
+			lis2dw12_acceleration_raw_get(&lis2dw12, raw_acceleration);
+		}
+		xQueueSend(xVisualQueueHandle, &raw_acceleration[0], 0);
+		static uint8_t cnt;
+		if (++cnt > 20) {
+			printf("X=%d Y=%d Z=%d\n", raw_acceleration[0],
+					raw_acceleration[1], raw_acceleration[2]);
+			cnt = 0;
+		}
+		vTaskDelay(50);
+		/* USER CODE END StartAcceleroTask */
+	}
+}
 /**
  * @brief  This function is executed in case of error occurrence.
  * @retval None
