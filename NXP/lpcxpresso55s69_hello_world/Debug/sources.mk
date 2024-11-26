0020dc40b6756a4599202f87a08a278254de60e7
Čipl Jan (240600) <240600@vutbr.cz>
2024-11-26 17:46:06 +0100

    LED and AD

---
+++ NXP/lpcxpresso55s69_hello_world/Debug/sources.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+OBJS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+utilities \
+
